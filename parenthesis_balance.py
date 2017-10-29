160a53433b40bf738fd48e1c0707a77224f68cfe
Aniruddha Adhikary <aniruddha@adhikary.net>
2017-10-29 09:02:43 +0600

    do not use another class for stack

--- parenthesis_balance.py
+++ parenthesis_balance.py
@@ -1,40 +1,22 @@
-class Stack:
-    def __init__(self):
-        self.items = []
-
-
-    def push(self, item):
-        self.items.append(item)
-
-
-    def pop(self):
-        return self.items.pop()
-
-
-    def is_empty(self):
-        if self.items == []:
-            return True
-        return False
-
-
 def is_balanced(input_str):
-    s = Stack()
+    s = list()
+
     for ch in input_str:
         if ch == '(':
-            s.push(ch)
+            s.append(ch)
         if ch == ')':
-            if s.is_empty():
+            if not s:
                 return False
             previous_ch = s.pop()
             if previous_ch != '(':
                 return False
-    if s.is_empty():
-        return True
-    return False
+
+    return not s
 
 
 if __name__ == "__main__":
     input_str = input() 
+
     if is_balanced(input_str):    
         print(input_str, "is balanced.")
     else:
