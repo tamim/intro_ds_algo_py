6bebfefabbdcd68957ff863508f06e38d7a12e34
রবিউল ইসলাম <32502709+rabiulislam993@users.noreply.github.com>
2017-11-18 11:25:40 +0600

    'if swapped' condition added.
    added condition to check if tow elements was swapped in current iteration.
    to eliminate extra loop from the sorted list.

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,13 @@
 def bubble_sort(L):
     n = len(L)
     for i in range(0, n):
+        swapped = False
         for j in range(0, n-i-1):
             if L[j] > L[j+1]:
                 L[j], L[j+1] = L[j+1], L[j]
+                swapped = True
+        if not swapped:
+            break
 
 if __name__ == "__main__":
     L = [6, 1, 4, 9, 2]
